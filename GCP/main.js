19bd463888cd4bf37b58e642719f61cd0e5af4ae
Sujal Ahrodia <sujalahrodia@gmail.com>
2019-01-22 00:52:32 -0500

    updated main.js

--- GCP/main.js
+++ GCP/main.js
@@ -2,25 +2,9 @@
 const Compute = require('@google-cloud/compute');
 const compute = new Compute();
 
-// Creates a new VM using the latest Ubuntu image.
-// (async () => {
-//   try {
-//     const zone = await compute.zone('us-central1-a');
-//     const data = await zone.createVM(
-//       'ubuntu-instance', 
-//       { os: 'ubuntu' }
-//     );
-//     const operation = data[1];
-//     await operation.promise();
-//     // Virtual machine created.
-//   } catch (error) {
-//     console.error(error);
-//   }
-// })();
-
-
 class GCPProvider
 {
+    // Creates a new VM using the latest Ubuntu image.
     async createInstance()
     {
         try {
@@ -36,6 +20,10 @@ class GCPProvider
         console.error(error);
         }
     }
+    async printInfo()
+	{
+		//To do
+	}
 };
 
 async function provision()
@@ -43,6 +31,7 @@ async function provision()
     let client = new GCPProvider();
 
     await client.createInstance();
+    //await client.printInfo();
 }
 
 (async () => {
